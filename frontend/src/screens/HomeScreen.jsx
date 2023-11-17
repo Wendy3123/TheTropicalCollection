4119d6d7b93a24533847c362d23d65e898304d2b
Rivka Weingarten <rw240brooklyn@gmail.com>
2023-11-16 20:35:40 -0500

    initial commit to Rivka1

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function HomeScreen() {
   return <div>
     
-    <img src="https://thetropicalcollection.com/fruit-dish.jpeg" href="jar of fruits"> </img></div>;
+    <img src="https://thetropicalcollection.com/fruit-dish.jpeg" href="jar of fruits Beautiful"> </img></div>;
 }
 
 export default HomeScreen;
