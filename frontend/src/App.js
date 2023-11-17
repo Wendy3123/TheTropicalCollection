42e48753878a30b9be851e1f040dcf0f3bd2a5ed
Wendy Wang <wendywangg928@gmail.com>
2023-11-16 21:25:22 -0500

    added products.js for mock items for now and added productscreen to display image and 26885$ for now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <h1>Welcome to The Tropical Collection</h1>
         </Container>
       </main>
+      <ProductScreen />
       <Footer />
     </>
   );
