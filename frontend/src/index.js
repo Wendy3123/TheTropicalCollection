e959693fb4321e89ff61107b885de58b750de54f
Rivka Weingarten <rw240brooklyn@gmail.com>
2023-11-19 15:59:51 -0500

    connected front end to backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "bootstrap/dist/css/bootstrap.min.css"; //default bootstrap styles
+// import "bootstrap/dist/css/bootstrap.min.css"; //default bootstrap styles
 
 
 
--- frontend/src/screens/EachProductScreen.jsx
+++ frontend/src/screens/EachProductScreen.jsx
@@ -2,11 +2,20 @@ import { useState, useEffect, UseParams } from "react";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
-import products from "../products";
+
 
 function EachProductScreen() {
-  const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
+  const [product, setProduct]  = useState({});
+  const{ id: productId} = useParams()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(`/api/products/${productId}`);
+      const resData = await res.json();
+      setProduct(resData);
+    };
+    fetchData();
+  }, [productId]);
 
   return (
     <>
@@ -15,7 +24,7 @@ function EachProductScreen() {
       </Link>
       <Row>
         <Col md={5}>
-          <Image src={product.image} alt={product.name} fluid></Image>
+        <Image className = "each-product-image" src={product.image} alt={product.name} fluid></Image>  
         </Col>
         <Col md={5}>
           <ListGroup variant="flush">
