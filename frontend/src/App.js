0f0e5a68c82e01da082cf7781db8ea43836bb68a
Wendy Wang <wendywangg928@gmail.com>
2023-11-21 18:09:37 -0500

    pushing changes to pull

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,21 +11,25 @@ import CartScreen from "./screens/CartScreen";
 import "./styles/bootstrap.custom.css";
 import "./styles/index.css";
 import "./styles/cards.css";
+import { ShopContextProvider } from "./context/ShopContext";
 
+//all components now have access to shopContextProvider
 function App() {
   return (
     <div className="backgroundimage">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<HomeScreen />}></Route>
-          <Route path="/cart" element={<CartScreen />}></Route>
-          <Route path="/products" element={<ProductScreen />}></Route>
-          <Route path="/products/:id" element={<EachProductScreen />}></Route>
-          <Route path="*" element={<ErrorScreen />}></Route>
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <ShopContextProvider>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomeScreen />}></Route>
+            <Route path="/cart" element={<CartScreen />}></Route>
+            <Route path="/products" element={<ProductScreen />}></Route>
+            <Route path="/products/:id" element={<EachProductScreen />}></Route>
+            <Route path="*" element={<ErrorScreen />}></Route>
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </ShopContextProvider>
     </div>
   );
 }
