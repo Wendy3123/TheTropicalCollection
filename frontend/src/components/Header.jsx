7ce0f623ae463f962c636ed7b10e8aa3f2713681
aminatol <aminatoluwo@gmail.com>
2023-11-20 21:31:59 -0500

    navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+   return (
+     <nav className="navbar">
+       <ul>
+         <li></li>
+         <li>
+           <a href="/" rel="Home Page">
+             Home
+           </a>
+         </li>
+         <li>
+           <a href="/" rel="Products">
+             Our Products
+           </a>
+         </li>
+         <li>
+           <a href="/Product/new" rel="Add Product">
+             Add New Product
+           </a>
+         </li>
+         <li>
+           <a href="/login" rel="login">
+             Login
+           </a>
+         </li>
+       </ul>
+
+       <p className="">
+         <span></span>
+       </p>
+     </nav>
+   );
 }
 
 export default Header;
