65d9d7d4f87f3e746a224bab2d1de7fda66d992e
Wendy Wang <wendywangg928@gmail.com>
2023-11-21 22:14:39 -0500

    added changes trying to solve rivkas issue

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
+import bodyparser from "body-parser";
 import connectDB from "./config/db.js";
 connectDB();
 import router from "./controllers/productsRoutes.js";
 import userRouter from "./controllers/usersRoutes.js";
 import authRouter from "./controllers/authRoutes.js";
 
-
 const app = express();
+app.use(bodyparser.json({ extended: true, limit: "20mb" }));
+app.use(bodyparser.urlencoded({ extended: true, limit: "20mb" }));
+app.use(cors());
 app.use("/api/products", router);
 app.use("/api/users", userRouter);
 app.use("/api/authentication", authRouter);
-
 
 // Create a homepage route.
 app.get("/", function (req, res) {
--- frontend/src/screens/SignUpScreen.jsx
+++ frontend/src/screens/SignUpScreen.jsx
@@ -1,37 +1,35 @@
-import { useState, useEffect } from "react"
-import { useNavigate, useParams } from "react-router"
-import {Link} from 'react-router-dom'
-import "../styles/login.css"
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router";
+import { Link } from "react-router-dom";
+import "../styles/login.css";
 
 function SignUpScreen() {
+  const navigate = useNavigate();
 
-	const navigate =useNavigate()
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-	const [user, setUser] = useState({
-		name: '',
-		email: '',
-		password: ''
-        
-	})
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-	async function handleSubmit(e) {
-		e.preventDefault()
+    await fetch(`/api/users/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
 
-		await fetch(`/api/users/`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(user)
-		})
-
-		navigate(`/`)
-	}
-    return (
-        <div className="login-top-container">
-         <div className="login-container">
-           <h1>Please Register</h1>
-            {/* {errorMessage !== null
+    navigate(`/`);
+  }
+  return (
+    <div className="login-top-container">
+      <div className="login-container">
+        <h1>Please Register</h1>
+        {/* {errorMessage !== null
                      ? (
                          <div className="alert alert-danger" role="alert">
                              {errorMessage}
@@ -39,50 +37,51 @@ function SignUpScreen() {
                      )
                      : null
                  }  */}
-           <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name</label>
+          <div className="form-control">
+            <input
+              type="name"
+              required
+              //  value={user.name}
+              onChange={(e) => setUser({ ...user, name: e.target.value })}
+              // className="form-control"
+              id="name"
+              name="name"
+            />
+          </div>
+          <label htmlFor="email">Email</label>
+          <div className="form-control">
+            <input
+              type="email"
+              required
+              //  value={user.email}
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+              // className="form-control"
+              id="email"
+              name="email"
+            />
+          </div>
 
-           <label htmlFor="name">Name</label>
-                 <div className="form-control">
-               <input type="name" 
-                 required
-                //  value={user.name}
-                 onchange={e => setUser({...user, name: e.target.value})}
-                 // className="form-control"
-                 id="name"
-                 name="name"
-                 />
-                 </div>
-           <label htmlFor="email">Email</label>
-                 <div className="form-control">
-               <input type="email" 
-                 required
-                //  value={user.email}
-                 onchange={e => setUser({...user, email: e.target.value})}
-                 // className="form-control"
-                 id="email"
-                 name="email"
-                 />
-                 </div>
-                 
-            <label>Password</label>
-            <div className="form-control">
-            <input type="password" 
-                 required
-                //  value={user.password}
-                 onchange={e => setUser({...user, password: e.target.value})}
-                 // className="form-control"
-                 id="password"
-                 name="password"
-                 />
-               </div>
-                    <button className="login-button">Sign Up</button>
-     
-            
-           </form>
-         </div>
-         </div>
-        
-       )
-     }
+          <label>Password</label>
+          <div className="form-control">
+            <input
+              type="password"
+              required
+              //  value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+              // className="form-control"
+              id="password"
+              name="password"
+            />
+          </div>
+          <button className="login-button" type="submit">
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
 
-     export default SignUpScreen
+export default SignUpScreen;
