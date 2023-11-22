aa2d0d6a6b4868e74b08d7079c08de75d7fcc979
aminatol <aminatoluwo@gmail.com>
2023-11-21 21:18:27 -0500

    adjustments to addnew css andjsx

--- frontend/src/components/AddNew.jsx
+++ frontend/src/components/AddNew.jsx
@@ -26,61 +26,64 @@ function AddNew() {
     navigate("/products");
   }   
   return (
-    <main>
+    <main className="form-main-container">
       <h1>Add New Product</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="add-product">
-          <label htmlFor="name">Product Name</label>
-          <input
-            required
-            value={product.name}
-            onChange={(e) => setProduct({ ...product, name: e.target.value })}
-            className="form"
-            id="name"
-            name="name"
-          />
-          <div className="form">
-            <label htmlFor="product-category">Choose Product Category</label>
-            <select
-              required
-              value={product.category}
-              onChange={(e) =>
-                setProduct({ ...product, category: e.target.value })
-              }
-              className="form"
-              id="prod-category"
-              name="prod-category"
-              defaultValue={product.name}
-            >
-              <option value="cat-1"> Category 1</option>
-              <option value="cat-2"> Category 2</option>
-              <option value="cat-3"> Category 3</option>
-              <option value="cat-4"> Category 4</option>
-            </select>
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <div className="add-product">
+            <label htmlFor="name">Product Name</label>
             <input
               required
-              value={product.description}
-              onChange={(e) =>
-                setProduct({ ...product, description: e.target.value })
-              }
+              value={product.name}
+              onChange={(e) => setProduct({ ...product, name: e.target.value })}
               className="form"
-              id="description"
-              name="description"
+              id="name"
+              name="name"
             />
-            <input
-              required
-              value={product.price}
-              onChange={(e) =>
-                setProduct({ ...product, price: e.target.value })
-              }
-              className="form"
-              id="price"
-              name="price"
-            />
-            <button className="submit"> Done</button>
+            <div className="form">
+              <label htmlFor="product-category">Choose Product Category</label>
+              <select
+                required
+                value={product.category}
+                onChange={(e) =>
+                  setProduct({ ...product, category: e.target.value })
+                }
+                className="form"
+                id="prod-category"
+                name="prod-category"
+                defaultValue={product.name}
+              >
+                <option value="cat-1"> Category 1</option>
+                <option value="cat-2"> Category 2</option>
+                <option value="cat-3"> Category 3</option>
+                <option value="cat-4"> Category 4</option>
+              </select>
+              <label htmlFor="product-category"> Description</label>
+              <input
+                required
+                value={product.description}
+                onChange={(e) =>
+                  setProduct({ ...product, description: e.target.value })
+                }
+                className="form"
+                id="description"
+                name="description"
+              />
+              <input
+                required
+                value={product.price}
+                onChange={(e) =>
+                  setProduct({ ...product, price: e.target.value })
+                }
+                className="form"
+                id="price"
+                name="price"
+              />
+              <button className="submit"> Done</button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </main>
   );
 }
