9c98a666c899fb7eb4e4bee997c7c6c9bff1a249
Wendy Wang <wendywangg928@gmail.com>
2023-11-21 20:28:28 -0500

    changed seeders to correct data and added cartItems array to userModel so we can now add specific itemID to the array

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,72 +1,76 @@
-
-import { useContext, useState } from "react"
-import { useNavigate } from "react-router"
-import { CurrentUser } from "../contexts/CurrentUser.js"
-import {Link} from 'react-router-dom'
-import "../styles/login.css"
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router";
+import { CurrentUser } from "../contexts/CurrentUser.js";
+import { Link } from "react-router-dom";
+import "../styles/login.css";
 
 function LoginScreen() {
-   
-        const navigate = useNavigate()
-    
-        const { setCurrentUser } = useContext(CurrentUser)
-    
-        const [credentials, setCredentials] = useState({
-            email: '',
-            password: ''
-        })
-    
-        const [errorMessage, setErrorMessage] = useState(null)
-    
-        async function handleSubmit(e) {
-            e.preventDefault()
-           
-            }
+  const navigate = useNavigate();
+
+  const { setCurrentUser } = useContext(CurrentUser);
+
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-   <div className="login-top-container">
-    <div className="login-container">
-      <h1>Please Login</h1>
-      {errorMessage !== null
-                ? (
-                    <div className="alert alert-danger" role="alert">
-                        {errorMessage}
-                    </div>
-                )
-                : null
-            }
-      <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Email</label>
-            <div className="form-control">
-          <input type="email" 
-            required
-            value={credentials.email}
-            onchange={e => setCredentials({...credentials, email: e.target.value})}
-            // className="form-control"
-            id="email"
-            name="email"
-            />
-            </div>
-            
-       <label>Password</label>
-       <div className="form-control">
-       <input type="password" 
-            required
-            value={credentials.password}
-            onchange={e => setCredentials({...credentials, password: e.target.value})}
-            // className="form-control"
-            id="password"
-            name="password"
+    <div className="login-top-container">
+      <div className="login-container">
+        <h1>Please Login</h1>
+        {errorMessage !== null ? (
+          <div className="alert alert-danger" role="alert">
+            {errorMessage}
+          </div>
+        ) : null}
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <div className="form-control">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              required
+              value={credentials.email}
+              onchange={(e) =>
+                setCredentials({
+                  ...credentials,
+                  [e.target.name]: e.target.value,
+                })
+              }
+              // className="form-control"
             />
           </div>
-               <button className="login-button">Login</button>
 
-        <p className="login-text">Don't have an account? 
-       <Link to = "/signup/" >Register</Link> </p>
-      </form>
+          <label>Password</label>
+          <div className="form-control">
+            <input
+              type="password"
+              required
+              value={credentials.password}
+              onchange={(e) =>
+                setCredentials({ ...credentials, password: e.target.value })
+              }
+              // className="form-control"
+              id="password"
+              name="password"
+            />
+          </div>
+          <button className="login-button">Login</button>
+
+          <p className="login-text">
+            Don't have an account?
+            <Link to="/signup/">Register</Link>{" "}
+          </p>
+        </form>
+      </div>
     </div>
-    </div>
-   
-  )
+  );
 }
 
-export default LoginScreen
+export default LoginScreen;
