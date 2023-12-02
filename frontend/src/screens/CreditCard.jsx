e0d4bea165216ad5deef34ccb2905f18d438b0e0
aminatol <aminatoluwo@gmail.com>
2023-12-02 16:56:48 -0500

    adjustments to creditcard form and css

--- frontend/src/screens/CreditCard.jsx
+++ frontend/src/screens/CreditCard.jsx
@@ -41,126 +41,134 @@ function CreditCard() {
   return (
     <div className="card-container">
       {!isSubmitted ? (
-        <form onSubmit={handleSubmit}>
+        <main>
           <h1>Payment Information</h1>
-          <div className="row">
-            <div className="first-last">
-              <div className="input-group">
-                <label htmlFor="firstName" className="form-label">
-                  First Name
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  value=""
-                  id=""
-                  placeholder=""
-                  //   onChange={(e) => setpayment(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="input-group">
-                <label htmlFor="lastName" className="form-label">
-                  Last Name
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  value=""
-                  id="lastName"
-                  placeholder=""
-                  //   onChange={(e) => setpayment(e.target.value)}
-                  required
-                />
-              </div>
-            </div>
+          <form onSubmit={handleSubmit}>
             <div className="row">
-              <div className="">
-                <label htmlFor="card-number" className="">
-                  Card Number
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="card-number"
-                  placeholder="1234 5678 9012 4563"
-                />
+              <div className="first-last">
+                <div className="input-group">
+                  <label htmlFor="firstName" className="form-label">
+                    first Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    value=""
+                    id=""
+                    placeholder="First Name"
+                    //   onChange={(e) => setpayment(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="lastName" className="form-label">
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    value=""
+                    xs
+                    id="lastName"
+                    placeholder="last name"
+                    //   onChange={(e) => setpayment(e.target.value)}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="row">
+                <div className="">
+                  <label htmlFor="card-number" className="">
+                    Card Number
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="card-number"
+                    placeholder="1234 5678 9012 4563"
+                  />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="">
-            <div className="col-sm-6">
+            <div className="">
+              <div className="col-sm-6">
+                <div className="">
+                  <label htmlFor="exp-date" className="">
+                    Expiration Date
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="exp-Date"
+                    placeholder="MM/YY"
+                  />
+                </div>
+              </div>
+
               <div className="">
-                <label htmlFor="exp-date" className="">
-                  Expiration Date
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="exp-Date"
-                  placeholder="MM/YY"
-                />
+                <div className="">
+                  <label htmlFor="cvv" className="">
+                    CVV
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="cvv"
+                    placeholder="456"
+                  />
+                </div>
               </div>
             </div>
 
+            {/* Billing  */}
             <div className="">
-              <div className="">
-                <label htmlFor="cvv" className="">
-                  CVV
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="cvv"
-                  placeholder="456"
-                />
-              </div>
-            </div>
-          </div>
-
-          {/* Billing  */}
-          <div className="">
-            <label htmlFor="billing" className="">
-              Billing Address
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="billing"
-              placeholder="123 Name St"
-            />
-          </div>
-          <div className="">
-            <div className="">
-              <div className="">
-                <label htmlFor="city" className="">
-                  City
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="city"
-                  placeholder="City "
-                />
-              </div>
+              <label htmlFor="billing" className="">
+                Billing Address
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="billing"
+                placeholder="123 Name St"
+              />
             </div>
             <div className="">
               <div className="">
-                <label htmlFor="zip" className="">
-                  Zip Code
-                </label>
-                <input type="text" className="" id="zip" placeholder="11111" />
+                <div className="">
+                  <label htmlFor="city" className="">
+                    City
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="city"
+                    placeholder="City "
+                  />
+                </div>
+              </div>
+              <div className="">
+                <div className="">
+                  <label htmlFor="zip" className="">
+                    Zip Code
+                  </label>
+                  <input
+                    type="text"
+                    className=""
+                    id="zip"
+                    placeholder="11111"
+                  />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="submit-container">
-            <Container>
-              <Button type="submit" className="submit-button" to="/">
-                Submit Payment
-              </Button>
-            </Container>
-          </div>
-        </form>
+            <div className="submit-container">
+              <Container>
+                <Button type="submit" className="submit-button" to="/">
+                  Submit Payment
+                </Button>
+              </Container>
+            </div>
+          </form>
+        </main>
       ) : (
         <div className="confirmation-message">Payment Confirmed</div>
       )}
